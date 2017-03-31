b9e7fa7810771947cf48e75997a69522c65f3918
k-schmidt <kyle.a.schmidt@gmail.com>
2017-03-31 17:54:30 -0400

    adding regex for ip_address

--- src/config.py
+++ src/config.py
@@ -8,3 +8,5 @@ PATH_SRC = os.path.dirname(__file__)
 PATH_ROOT = os.path.dirname(PATH_SRC)
 PATH_LOG_INPUT = os.path.join(PATH_ROOT, "log_input")
 PATH_LOG_INPUT_FILE = os.path.join(PATH_LOG_INPUT, "log.txt")
+
+regex_ip_address = r"(?P<ip_address>[^\s]+)"
