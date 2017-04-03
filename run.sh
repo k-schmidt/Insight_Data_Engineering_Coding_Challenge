cec92e11be186371546002b582f4dae1bd6c4f0a
k-schmidt <kyle.a.schmidt@gmail.com>
2017-04-03 12:34:45 -0400

    small modifications to bash scripts

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
-
-# one example of run.sh script for implementing the features using python
-# the contents of this script could be replaced with similar files from any major language
+#############################################
+# Run Coding Challenge
+#
+# Kyle Schmidt
+# Insight Data Engineering Coding Challenge
+#############################################
 
 # I'll execute my programs, with the input directory log_input and output the files in the directory log_output
 python3 ./src/main.py
