acbc618f5f44a0356e65da2429fcb96859efc334
k-schmidt <kyle.a.schmidt@gmail.com>
2017-04-04 18:05:05 -0400

    adding Trie and Node documentation

--- src/pkg/trie.py
+++ src/pkg/trie.py
@@ -4,20 +4,48 @@ Trie Implementation
 Kyle Schmidt
 Insight Data Engineering Coding Challenge
 """
-from typing import Optional
+from typing import Optional, Tuple
 
 
 class Node(dict):
 
     def __init__(self, label: Optional[str]=None):
-        self.label = label
-        self.count = 0
-        self.is_in_heap = False
+        """
+        Node class representing a value in the Trie class.
+        Node inherits from dict providing dictionary functionality
+        while being able to store additional data
 
-    def add_child(self, key: str):
+        Attributes:
+            label: Character of word within Trie
+            count: Number of times the word has been seen
+                   Count is only incremented when we reach the end
+                   of a word.
+            is_in_heap: Boolean indicator letting us know when to
+                        traverse a heap looking for the node
+        """
+        self.label = label
+        self.count = 0  # type: int
+        self.is_in_heap = False  # type: bool
+
+    def add_child(self, key: str) -> None:
+        """
+        Add a value to the Node which itself is another Node
+
+        Arguments:
+            key: Character of word
+        """
         self[key] = Node(key)
 
-    def increment_priority(self, priority_incrementer: int):
+    def increment_priority(self, priority_incrementer: int) -> None:
+        """
+        Nodes will also be stored in a heap and we need to
+        be able to update a Nodes priority within that heap
+
+        Priority Incrementer tells us how much to increment the priority by
+
+        Arguments:
+            priority_incrementer: Number to increment instance attribute count
+        """
         self.count += priority_incrementer
 
     def __repr__(self):
@@ -35,9 +63,33 @@ class Node(dict):
 class Trie:
 
     def __init__(self):
-        self.head = Node()
+        """
+        Class used as an ADT of Trie
 
-    def add(self, item: str, priority_incrementer: int=1):
+        A Trie contains Nodes which contain characters of a word and pointers
+        to additonal characters of words.
+
+        Attributes:
+            head: Empty Node instance
+        """
+        self.head = Node()  # type: Node
+
+    def add(self, item: str, priority_incrementer: int=1) -> Tuple[Node, str]:
+        """
+        Add a word to the Trie by traversing the characters of that word.
+        If the word is already in the Trie then we will visit of the
+        Nodes representing that word.
+        Otherwise we will create new Nodes to represent that word.
+        Finally we will increment the counter for that word.
+
+        Arguments:
+            item: The word to add to the Trie
+            priority_incrementer: Number to increase the Node's count attribute
+
+        Returns:
+            Node instance representing the last character of the word and
+            the word itself
+        """
         current_node = self.head
         item_finished = True
 
